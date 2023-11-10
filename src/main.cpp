9f536013750be5c562fad8ef6ef17d8b0f5fb0f0
serweryn617 <seweryn.r.1999@gmail.com>
2023-11-10 17:01:24 +0100

    Add namespace in main

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@
 // based on example from: https://www.nongnu.org/lwip/2_0_x/group__lwip__nosys.html
 #define ETHERNET_MTU 1500
 
-
+using namespace devices::enc28j60;
 uint8_t mac_addr[6] = {0x11, 0xe8, 0xc3, 0xf8, 0xc6, 0x92};
 
 PicoSPIDriver enc_driver(SPI_PORT, PIN_CS, PIN_MISO, PIN_MOSI, PIN_SCK);
