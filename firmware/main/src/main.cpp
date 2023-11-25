dc91c372d0afb92ce7d0fd47ee1c62a8baa7590e
serweryn617 <seweryn.r.1999@gmail.com>
2023-11-25 17:13:42 +0100

    Update udp receiver

--- firmware/main/src/main.cpp
+++ firmware/main/src/main.cpp
@@ -35,7 +35,7 @@ int main()
     INA219 ina219(i2c_driver);
     ina219.calibrate();
 
-    uint8_t mac_addr[6] = { 0x11, 0xe8, 0xc3, 0xf8, 0xc6, 0x92 };
+    uint8_t mac_addr[6] = { 0x11, 0xe8, 0xc3, 0xf8, 0xc6, 0x92 };  // TODO: remove
     PicoSPIDriver spi_driver(spi1, hw::enc28j60::spi::cs, hw::enc28j60::spi::miso, hw::enc28j60::spi::mosi, hw::enc28j60::spi::sck);
     spi_driver.init();
     Enc28j60 enc28j60(spi_driver);
@@ -43,37 +43,39 @@ int main()
     Interface network_interface(enc28j60);
     network_interface.init();
 
+    uint8_t udp_rcv_data[10] = {0};
+    network_interface.set_udp_rcv_buffer(udp_rcv_data, sizeof(udp_rcv_data));
+
     while (true) {
-        // sw = !sw;
-        // sleep_ms(100);
-        // gpio_put(mosfet_pin, sw);
-        // sleep_ms(100);
-
-        // float sh = ina219.shunt_voltage();
-        // printf("Shunt voltage: %.2f mV\n", sh);
-
-        // float bus = ina219.bus_voltage();
-        // printf("Bus voltage: %.2f V\n", bus);
-
         network_interface.receive();
 
-        uint16_t bus = ina219.bus_voltage_raw();
-        uint16_t shunt = ina219.shunt_voltage_raw();
-        uint16_t current = ina219.current_raw();
+        if (uint16_t len = network_interface.get_udp_rcv_len(); len >= 5) {
+            if (udp_rcv_data[0] == 'S' && udp_rcv_data[1] == 'M' && udp_rcv_data[2] == 'N' && udp_rcv_data[3] == 'S') {
+                if (udp_rcv_data[4] == '0') {
+                    gpio_put(mosfet_pin, false);
+                }
+                if (udp_rcv_data[4] == '1') {
+                    gpio_put(mosfet_pin, true);
+                }
+                if (udp_rcv_data[4] == 'R') {
+                    uint16_t bus = ina219.bus_voltage_raw();
+                    uint16_t shunt = ina219.shunt_voltage_raw();
+                    uint16_t current = ina219.current_raw();
 
-        uint8_t data[10] = {'S', 'M', 'N', 'S'};  // magic
-        data[4] = bus & 0xff;
-        data[5] = bus >> 8;
-        data[6] = shunt & 0xff;
-        data[7] = shunt >> 8;
-        data[8] = current & 0xff;
-        data[9] = current >> 8;
+                    uint8_t data[10] = {'S', 'M', 'N', 'S'};  // magic
+                    data[4] = bus & 0xff;
+                    data[5] = bus >> 8;
+                    data[6] = shunt & 0xff;
+                    data[7] = shunt >> 8;
+                    data[8] = current & 0xff;
+                    data[9] = current >> 8;
 
-        network_interface.send(data, sizeof(data));
+                    network_interface.send(data, sizeof(data));
+                }
+            }
+        }
 
         network_interface.check_timers();
-
-        sleep_ms(1000);
     }
 
     return 0;
